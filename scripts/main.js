27e307785e749ab72c2ffdd395632a2e1a2f3c65
Web <web@it.step>
2021-08-12 19:58:23 +0300

    implemented getDistance function

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+const hint = document.getElementById('hint');
+const map = document.querySelector('[alt="map"]');
+
+// function getDistanceToTreasure (clickCoords, treasureCoords) {
+//     return Math.sqrt(Math.pow((clickCoords.x-treasureCoords.x),2)+Math.pow(clickCoords.y-treasureCoords.y,2))
+// }
+// console.log(getDistanceToTreasure({x: 2, y : 2},{x:5, y : 4}));
+// либо с деструктуризацией
+function getDistanceToTreasure ({x : cX, y : cY},{x : tX, y : tY} ) // деструктуризация объектов с переименовыванием
+{
+    return Math.sqrt((cX - tX)**2 + (cY - tY)**2);
+}
