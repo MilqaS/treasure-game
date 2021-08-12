f8f82cb43f3f4c17f9a84dffcef7cd65ceb567c6
Web <web@it.step>
2021-08-12 21:18:12 +0300

    divided module

---
+++ scripts/utils/generateTreasureCoords.js
@@ -0,0 +1,9 @@
+import { generateRandomNumber } from "./generateRandomNumber.js";
+
+
+const generateTreasureCoords = ({ width, height }, gap) => ({
+    x: generateRandomNumber(width, gap),
+    y: generateRandomNumber(height, gap)
+}); 
+
+export {generateTreasureCoords};
