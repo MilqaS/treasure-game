37e8c070d12b309839991c9652ce3c482e1c09a2
Web <web@it.step>
2021-08-15 20:51:17 +0300

    implemented game logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,5 @@
-import {generateTreasureCoords} from "./utils/generateTreasureCoords.js";
-import { getDistanceToTreasure } from "./utils/getDistanceToTreasure.js";
+import {mapClickHandler} from './utils/gameHelpers.js';
 
-const hint = document.getElementById('hint');
 const treasureMap = document.querySelector('[alt="map"]');
 
-
-
-// или переименовать импортируемую функцию в пределах этого файла
-// import {generateRandomNumber as myFunc} from "./utils/generateRandomNumber.js";
-// const hint = document.getElementById('hint');
-// const treasureMap = document.querySelector('[alt="map"]');
-// console.log(myFunc(200,20));
+treasureMap.addEventListener('click', mapClickHandler);
